272f768772931add7f3fccf0abe4dce556e8822d
illumination-k <illumination.k.27@gmail.com>
2021-01-20 21:40:18 +0900

    feat: add mcl

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+extern crate anyhow;
+
+extern crate ndarray_stats;
+
+#[macro_use]
+extern crate ndarray;
+
+#[macro_use]
+extern crate ndarray_linalg;
+
+#[cfg(test)]
+#[macro_use]
+extern crate approx;
+
+pub mod mcl;
